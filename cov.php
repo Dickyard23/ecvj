4be52077c731fee839d1f61acee5b5abb192c88e
Dickyard23 <66295242+Dickyard23@users.noreply.github.com>
2020-06-05 13:26:59 +0700

    Add files via upload

---
+++ cov.php
@@ -0,0 +1,4 @@
+<?php
+    $uid = "EC-UserId-86408";
+    $key = "https://colet-auto-coins.xyz/faucet.php?adcopy_response=Picture%20perfect&adcopy_challenge=2@XDDHA25GVRFTVqvqfL1g7jPvCtu7L.YH@Wu2s-dxsWSUENU8bgrS8xX9APTC4lJjoYJ2H9bzDukRsnTRStW7QxTqV3THOcA7Os-14f.2y7-kkfUKfzVSBHseEpXtbNF821uwrxBKxFLeusKxYpbn8s2Jh3fue028y.mqssbk7zTQjkrT0LCAJvBMyQBYohO07P-68.P87D4ePDuiCwpoAKp6w5p5FVnMGzqytO4nUDK9v.31lcnfUpZ7aAIia-Bldm2p5NuDNEwfjv.R4woBP6-YkctCd3wIKz1JKkjJNP5cnGmn7LK0TzKmyarjxszGBJkhFXIK0uoA&address=EC-UserId-86408&currency=DOGE&key=e71ff3549532656ec3558563a7da19d3";
+    ?>
